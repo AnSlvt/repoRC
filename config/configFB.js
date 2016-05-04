c88119904b07725c60a4d9f7de4350073a459983
Andrea <andrea.slvt94@gmail.com>
2016-05-04 13:35:49 +0200

    First commit

---
+++ config/configFB.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Andrea on 15/12/2015.
+ */
+
+// this should really be in a config file!
+module.exports = {
+    client_id    :  '900202573407551'
+    , client_secret:  '4a607c136f0c4bb8dd6385bd37955533'
+    , scope        :  'user_friends'
+    , redirect_uri :  'http://localhost:3000/auth'
+};
---
+++ config/configTW.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Andrea on 15/12/2015.
+ */
+
+//To be obtained from the app created on Twitter
+const CONSUMER_KEY    = "Jz2WGDGqafKcxEqPlgOXl02mM"
+    , CONSUMER_SECRET = "Zd71swilBAO1Sk1HmqroCB4OsWILk5rjQP6CU7HALQEIASovaI";
+
+//Oauth Object to be used to obtain Request token and Access token from Twitter
+module.exports = {
+    callback        : "http://localhost:3000/signin-with-twitter"
+    , consumer_key    : CONSUMER_KEY
+    , consumer_secret : CONSUMER_SECRET
+};
